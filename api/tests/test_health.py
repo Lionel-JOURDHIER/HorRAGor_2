f13fc48180b1dace5f35ae0bb7b538315ab21fa0
Hanna Kiyko <kiyko.hanna1984@gmail.com>
2026-06-14 21:09:18 +0200

    tests

--- api/tests/test_health.py
+++ api/tests/test_health.py
@@ -1,4 +1,7 @@
 # tests/test_health.py
+from unittest.mock import MagicMock
+from api.main import app
+from database.connection import get_db
 
 def test_health(client):
 
@@ -9,3 +12,18 @@ def test_health(client):
     assert response.json() == {
         "status": "ok"
     }
+
+
+def test_health_exception(client):
+    mock_db = MagicMock()
+    mock_db.execute.side_effect = Exception("DB down")
+
+    # override dependency
+    app.dependency_overrides[get_db] = lambda: mock_db
+
+    response = client.get("/health")
+
+    assert response.status_code == 500
+    assert "Health check failed" in response.json()["detail"]
+
+    app.dependency_overrides.clear()
--- api/tests/test_metadata.py
+++ api/tests/test_metadata.py
@@ -1,5 +1,7 @@
 # tests/test_metadata.py
 
+from unittest.mock import patch
+
 def test_list_genre(client):
 
     response = client.get("/list_genre")
@@ -10,6 +12,15 @@ def test_list_genre(client):
 
     assert "genres" in data
     assert isinstance(data["genres"], list)
+
+def test_list_genre_exception(client):
+    with patch("api.routes.get_all_genres") as mock_get_all_genres:
+        mock_get_all_genres.side_effect = Exception("DB error")
+
+        response = client.get("/list_genre")
+
+        assert response.status_code == 500
+        assert "Failed to retrieve genres" in response.json()["detail"]
 
 
 def test_list_real(client):
@@ -23,3 +34,11 @@ def test_list_real(client):
     assert "directors" in data
     assert isinstance(data["directors"], list)
 
+def test_list_real_exception(client):
+    with patch("api.routes.get_all_directors") as mock_get_all_genres:
+        mock_get_all_genres.side_effect = Exception("DB error")
+
+        response = client.get("/list_real")
+
+        assert response.status_code == 500
+        assert "Failed to retrieve directors" in response.json()["detail"]
