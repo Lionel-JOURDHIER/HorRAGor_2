5587557d86a3f0d5d68c90fa59a78f3a2626a95f
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2026-07-30 15:43:51 +0200

    fix : Ollama connection

--- shared/embeddings.py
+++ shared/embeddings.py
@@ -1,5 +1,11 @@
+import os
+
+from dotenv import load_dotenv
 from langchain_ollama import OllamaEmbeddings
 
+load_dotenv()
+
 OLLAMA_CLIENT_EMBEDD = OllamaEmbeddings(
-    model="qwen3-embedding:0.6b"
+    model="qwen3-embedding:0.6b",
+    base_url=os.getenv("OLLAMA_BASE_URL", "http://host.docker.internal:11434"),
 )
